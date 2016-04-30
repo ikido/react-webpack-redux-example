a34c31a4dfacc1f0c040090ad4610c965def1987
Alexander Ponomarev <ikidoit@gmail.com>
2016-04-30 16:52:23 +0300

    added proper date formatting

--- javascripts/views/stories/show.jsx
+++ javascripts/views/stories/show.jsx
@@ -2,6 +2,7 @@
 
 import React, { Component, PropTypes } from 'react';
 import ListGroupItem from 'react-bootstrap/lib/ListGroupItem';
+import moment from 'moment';
 
 export default class StoryList extends Component {
 
@@ -15,7 +16,7 @@ export default class StoryList extends Component {
       <ListGroupItem>
       	<a href="javascript:void(0)" onClick={ this.props.onClick }>{ this.props.story.title }</a>
       	<br />
-      	<small>{ new Date(this.props.story.date).toString() }</small>
+      	<small>{ moment(this.props.story.date).format('MMM D, YYYY [at] h:mma') }</small>
       </ListGroupItem>
     );
   }
